47dca078276675d448d166b3fc8395da6723240e
Zaeem Athar <zaeemathar94@gmail.com>
2024-03-05 13:56:21 +0100

    Updating cloud function code.

--- cloud_functions/main.py
+++ cloud_functions/main.py
@@ -2,6 +2,20 @@ from google.cloud import storage
 import functions_framework
 import dlt
 import json
+import threading
+
+# Access the content of the file
+client = storage.Client()
+
+# Semaphore to control pipeline execution
+pipeline_semaphore = threading.Semaphore(1)
+
+# Define the DLT pipeline configuration
+pipeline = dlt.pipeline(
+    pipeline_name = "tel_pipeline",
+    destination = "bigquery",
+    dataset_name = "telemetry_data"
+)
 
 # Triggered by a change in a storage bucket
 @functions_framework.cloud_event
@@ -12,29 +26,42 @@ def storage_bigquery(cloud_event):
     file_name = data["name"]
 
     # Access the content of the file
-    client = storage.Client()
-
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(file_name)
 
-    file_content = blob.download_as_text()
+    try:
+        file_content = blob.download_as_bytes()
+        json_data = read_json_file(file_content)
 
-    # Assuming each line in the file is a separate JSON object
-    json_data = []
-    for line in file_content.splitlines():
-        try:
-            json_object = json.loads(line)
-            json_data.append(json_object)
-        except json.JSONDecodeError as e:
-            print(f"Error decoding JSON: {e}")
-            
+        with pipeline_semaphore:
+            # Run the DLT pipeline with the loaded JSON data
+            load_info = pipeline.run(telemetry_logs(json_data), write_disposition='append')
+            print(load_info)
 
-    pipeline = dlt.pipeline(
-        pipeline_name="quick_start",
-        destination="bigquery",
-        dataset_name="pub_sub_data"
-    )
+    except Exception as e:
+        print(f"Error processing file {file_name}: {str(e)}")
 
-    load_info = pipeline.run(json_data, write_disposition='append', table_name="events")
 
-    print(load_info)
+def read_json_file(file_content):
+    data = []
+    # Decode bytes-like object to string
+    file_content_str = file_content.decode('utf-8')
+    # Split data into lines
+    lines = file_content_str.split('\n')
+
+    for line in lines:
+        # Remove leading/trailing whitespaces
+        line = line.strip()
+        # Skip empty lines
+        if line:
+            try:
+                # Parse JSON
+                json_object = json.loads(line)
+                data.append(json_object)
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON on line {line}: {str(e)}")
+    return data
+
+@dlt.resource(table_name=lambda i: i["event"], write_disposition='append')
+def telemetry_logs(logs):
+    yield logs
